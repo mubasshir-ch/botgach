c288169d2b6894b43060219b65fdaff326c22c17
hackermub <44833007+hackermub@users.noreply.github.com>
2021-10-17 15:07:28 +0600

    Update video.py

--- botmodules/video.py
+++ botmodules/video.py
@@ -7,10 +7,10 @@ def get_op(start=40):
     ytd_op = {
         'verbose': True,
         'format': 'mp4',
-        "default_search": "ytsearch",
-        "quite": "True",
-        "extract_flat": "in_playlist",
-        'options': f'-vn -ss {start}
+        'default_search': 'ytsearch',
+        'quite': 'True',
+        'extract_flat': 'in_playlist',
+        'options': f'-vn -ss {start}'
     }
     return ytd_op
 
